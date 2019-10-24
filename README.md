6e7fb2acfa68f284c5d4adb5248f68c916d454e6
root <root@ip-172-31-30-47.us-west-2.compute.internal>
2019-10-24 05:03:07 +0000

    Comcast PoC Base commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Accenture Intelligent Orchestration Platform
+
+Ansible playbooks that are used for common orchestration tasks that are defined in an Ansible Tower installation.
+
+Playbooks are executed normally from Ansible Tower as a job template or can be run locally using normal ansible-playbook command line. Therefore all additional documentation will be found in the individual roles about there requirements or dependancies.
+
+## Ansible
+* Read how to use the **Ansible** provisioner in README.md of each role.
+* To know more about Ansible: http://www.ansible.com
+
+
+License
+-------
+
+Apache
+
+Author Information
+------------------
+
+Emerging Technologies
+# ComcastPoc
---
+++ filter_plugins/versa.py
@@ -0,0 +1,94 @@
+# (c) 2016, Jiri Tyr <jiri.tyr@gmail.com>
+#
+# This file is part of Config Encoder Filters (CEF)
+#
+# CEF is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# CEF is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CEF.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Config Encoder Filters
+More information: https://github.com/jtyr/ansible-config_encoder_filters
+"""
+
+from __future__ import (absolute_import, division, print_function)
+from ansible import errors
+from copy import copy
+import re
+import json
+
+
+# Required for Python 3 compatibility
+try:
+    basestring
+except NameError:
+    basestring = str
+
+
+def updateInterfaceList(xml):
+
+    updatedInterfaceList = []
+    portalInputs = xml["portalInputs"]
+    print("MKA UPDATE INTERFACE :: %s", portalInputs )
+    networkInfoList = xml["networkInfoList"]
+    for i in range(0, len(portalInputs)):
+        interfaceName = portalInputs[i]["portName"] +  "." + portalInputs[i]["subInterfaceId"] 
+        ipAddress = portalInputs[i]["ipAddress"]
+        for j in range(0, len(networkInfoList)):
+           networkName = networkInfoList[j]["name"]
+           for k in range(0, len(networkInfoList[j]["interfaces"])): 
+               tmpInterfaceName = networkInfoList[j]["interfaces"][k] 
+               print("COMPARING  %s with :: %s", interfaceName, tmpInterfaceName )
+               if ( interfaceName == tmpInterfaceName): 
+                  updateInterfaceItem = {}
+                  updateInterfaceItem["name"] = networkName 
+                  updateInterfaceItem["ipAddress"] = ipAddress 
+                  updatedInterfaceList.append(updateInterfaceItem)
+
+    return updatedInterfaceList
+
+
+
+def modifyInterfaceDetails(xml):
+
+    origMessage = xml["origMsg"]
+    interfaceList = xml["inputs"]
+    print("MKA INTERFACE LIST :: %s", interfaceList)
+#    stagingVariables = xml["origMsg"]["versanms.sdwan-device-workflow"]["stagingTemplateInfo"]["templateData"]["device-template-variable"]["variable-binding"]["attrs"]
+    postStagingVariables = xml["origMsg"]["versanms.sdwan-device-workflow"]["postStagingTemplateInfo"]["templateData"]["device-template-variable"]["variable-binding"]["attrs"]
+    print("MKA INTERFACE LIST2 :: %s", interfaceList)
+
+    for i in range(0, len( postStagingVariables)):
+        if ( postStagingVariables[i]["isAutogeneratable"] == 0 ):
+            if(re.search("staticaddress", postStagingVariables[i]["name"])):	
+                for k in range(0, len( interfaceList)):
+                    networkName = interfaceList[k]["name"]
+                    print("CHECKING FOR NETWORK :: %s", networkName)
+	            if ( re.search(networkName, postStagingVariables[i]["name"])):
+                        print("MATCHED NETWORK PATTERN")
+		        postStagingVariables[i]["value"] =  interfaceList[k]["ipAddress"]
+
+	
+    origMessage["versanms.sdwan-device-workflow"]["postStagingTemplateInfo"]["templateData"]["device-template-variable"]["variable-binding"]["attrs"]= postStagingVariables 
+    return origMessage
+
+
+class FilterModule(object):
+    """Ansible encoder Jinja2 filters."""
+
+    def filters(self):
+        """Expose filters to ansible."""
+
+        return {
+            'modifyInterfaceDetails': modifyInterfaceDetails,
+            'updateInterfaceList': updateInterfaceList
+        }
